f887503eb31919c9a95aa1da12bea3611c76d466
Jordy <jord_id@abv.bg>
2023-03-03 15:38:42 +0200

    test

--- source/main_menu.gd
+++ source/main_menu.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var create_button:= $VBoxContainer/VBoxContainer/CreateButton
 
 
-
+# I have edited the project look at me.
 func _ready():
 	pass # Replace with function body.
 
